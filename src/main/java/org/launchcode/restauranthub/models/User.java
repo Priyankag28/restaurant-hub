e8852e0c792b13d2af5d711f0b30164638d8da34
priyanka <priyanakangel28@gmail.com>
2021-05-05 15:50:11 -0500

    Login and Register Page

---
+++ src/main/java/org/launchcode/restauranthub/models/User.java
@@ -0,0 +1,39 @@
+package org.launchcode.restauranthub.models;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue
+    private int id;
+    private String username;
+    private String pwHash;
+
+    public User(){
+    }
+    private static final BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+    public User (String username, String password){
+        this.username = username;
+        this.pwHash = encoder.encode(password);
+    }
+    public String getUsername(){
+        return username;
+    }
+    public boolean isMatchingPassword(String password){
+        return encoder.matches(password, pwHash);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
